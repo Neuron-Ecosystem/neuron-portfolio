f2a5f7c5de85100bd676c2b5a193f90ee065e811
Neuron <wertq6306@gmail.com>
2026-01-10 23:53:51 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,39 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+import { getFirestore, doc, getDoc } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBIkhWk34e7LZS7EqfmeK31bahBm8if28c",
+    authDomain: "neuron-portfolio-2026.firebaseapp.com",
+    projectId: "neuron-portfolio-2026",
+    storageBucket: "neuron-portfolio-2026.firebasestorage.app",
+    messagingSenderId: "688199450932",
+    appId: "1:688199450932:web:f69ffc4bf901d67e392561",
+    measurementId: "G-C52KLDJRXP"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+document.getElementById('checkBtn').onclick = async () => {
+    const code = document.getElementById('inviteInput').value.trim();
+    if (!code) return;
+
+    try {
+        const docRef = doc(db, "invites", code);
+        const docSnap = await getDoc(docRef);
+
+        if (docSnap.exists()) {
+            const data = docSnap.data();
+            document.getElementById('inviteInput').style.display = 'none';
+            document.getElementById('checkBtn').style.display = 'none';
+            const msg = document.getElementById('msg');
+            msg.style.display = 'block';
+            msg.innerHTML = `ДОСТУП РАЗРЕШЕН<br>ПРИВЕТ, ${data.name.toUpperCase()}<br>СТАТУС: ELITE USER`;
+        } else {
+            alert("ОШИБКА: КОД НЕ ВЕРЕН");
+        }
+    } catch (e) {
+        console.error(e);
+        alert("Ошибка доступа");
+    }
+};
